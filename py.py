bbf95ebb8cf0c0f5f03178363dda83ccb111a949
jivilkapooja <poojajivilka@gmail.com>
2024-08-25 11:31:02 +0530

    py.py

--- py.py
+++ py.py
@@ -15,22 +15,22 @@ while True:
     choice = input("Enter choice(1/2/3/4): ")
     if choice in ('1', '2', '3', '4'):
         try:
-            num1 = float(input("Enter first number: "))
-            num2 = float(input("Enter second number: "))
+            A = float(input("Enter first number: "))
+            B = float(input("Enter second number: "))
         except ValueError:
             print("Invalid input. Please enter a number.")
             continue
         if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
+            print(A, "+", B, "=", add(A, B))
 
         elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
+            print(A, "-", B, "=", subtract(A, B))
 
         elif choice == '3':
-            print(num1, "/", num2, "=", divide(num1, num2))
+            print(A, "/", B, "=", divide(A, B))
 
         elif choice == '4':
-            print(num1, "*", num2, "=", multiply(num1, num2))
+            print(A, "*", B, "=", multiply(A, B))
         next_calculation = input("Let's do next calculation? (yes/no): ")
         if next_calculation == "no":
             break
